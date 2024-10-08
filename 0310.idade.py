9da97c7b8d299e3fbecb6d57769d7ec3909c5d17
Beatriz007 <113737395+BeatrizSales007@users.noreply.github.com>
2024-10-08 20:41:56 -0300

    Add files via upload

---
+++ 0310.idade.py
@@ -0,0 +1,8 @@
+idade = 15
+
+if idade < 16:
+    print("Você é menor de idade")
+elif 16 <= idade <= 18:
+    print("Você pode entrar, mas precisa esta acompanhado")
+else:
+    print("Pode entrar")
