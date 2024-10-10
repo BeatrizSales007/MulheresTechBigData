0c01d05e4e583deab0e34040c926320f824e05f9
Beatriz007 <113737395+BeatrizSales007@users.noreply.github.com>
2024-10-10 20:58:17 -0300

    ex05.py

--- 08_10/ex05.py
+++ 08_10/ex05.py
@@ -1,6 +1,6 @@
-# 5) Escreva um programa que leia as notas das duas avaliações normais e a nota da avaliação optativa dos estudantes de uma turma. Caso o estudante não tenha feito a optativa, deve ser fornecido o valor -1. Calcular a média do semestre considerando que a prova optativa substitui a 
-# nota mais baixa entre as duas primeiras avaliações. Escrever a média e mensagens que indiquem se o estudante foi aprovado, reprovado ou se 
-# está em exame, de acordo com as informações abaixo: 
+# 5) Escreva um programa que leia as notas das duas avaliações normais e a nota da avaliação optativa dos estudantes de uma turma. Caso o estudante não tenha feito a optativa, 
+# deve ser fornecido o valor -1. Calcular a média do semestre considerando que a prova optativa substitui a  nota mais baixa entre as duas primeiras avaliações.
+# Escrever a média e mensagens que indiquem se o estudante foi aprovado, reprovado ou se está em exame, de acordo com as informações abaixo: 
 # Aprovado: média >= 6.0, Reprovado: média < 3.0, Exame: média >= 3.0 e < 6.0;
 
 nota1 = float(input("Digite a nota da primeira avaliação: "))
