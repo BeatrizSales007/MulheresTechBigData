50f3516356284dc681e824777811c45eb510c01d
Beatriz007 <113737395+BeatrizSales007@users.noreply.github.com>
2024-10-11 20:47:03 -0300

    Add files via upload

---
+++ 11_10/atividade.py
@@ -0,0 +1,21 @@
+# Desenvolva um programa que guarde os dados de 10 pessoas que estão se candidatando a uma vaga de emprego, sabendo que candidatos 
+# menores de 18 anos não podem participar. Os dados coletados são: nome, data de nascimento, telefone, e-mail, formação acadêmica e a 
+# experiência profissional.
+
+while True:
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    
+    if idade < 18:
+        print("Menor de 18 anos não pode se candidatar.")
+        continue
+
+    telefone = input("Telefone: ")
+    email = input("E-mail: ")
+    formacao = input("Formação Acadêmica: ")
+    experiencia = input("Experiência Profissional: ")
+
+    print(f"\nCandidato cadastrado: Nome: {nome}, Idade: {idade}, Telefone: {telefone}, E-mail: {email}, Formação: {formacao}, Experiência: {experiencia}")
+    
+    if input("\nDeseja cadastrar mais um candidato? (s/n): ").lower() != 's':
+        break
